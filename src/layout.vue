6a0109d464d0b03083dce39051309c5e687d83eb
arman <armanlegand1396@gmail>
2020-10-25 15:05:30 +0330

    init-commit

---
+++ src/layout.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="body">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+	</div>
+</template>
+
+<script>
+	import HelloWorld
+		from './components/HelloWorld.vue';
+
+	export default {
+		name: 'layout',
+		components: {
+			HelloWorld
+		},
+		data()
+		{
+			return {};
+		},
+
+		methods: {},
+
+
+	};
+</script>
+<style scoped>
+
+	* {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+		border: 0;
+	}
+
+</style>
---
+++ src/server.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import router from './router';
+
+
+let serverURL = 'http://localhost/laravelyar/public/';
+
+const instance = axios.create({
+
+	baseURL: serverURL + 'api/v1/front/',
+	// baseURL: serverURL ,
+
+	// baseURL: 'http://qlake.ir/kanoon-back/public/api/',
+	// baseURL: 'http://kokerpanel.ir/back/public/api/',
+	//timeout: 1000,
+	//headers: {'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}
+
+});
+
+
+instance.interceptors.request.use(function(config)
+{
+	// Do something before request is sent
+	//np.start();
+	config.headers = {
+		"X-Requested-With": "XMLHttpRequest",
+		'Authorization': 'Bearer ' + localStorage.getItem('token'),
+		'sss': 'sss',
+
+	};
+	//config.crossdomain= true
+
+	window.app.errors = {};
+
+	return config;
+}, function(error)
+{
+	// Do something with request error
+	return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function(response)
+{
+	// Do something with response data
+	//np.done();
+	return response;
+}, function(error)
+{
+	// Do something with response error
+	if (error.response.status === 401)
+	{
+		//console.log(window.app)
+		localStorage.removeItem('token');
+		router.push({path: '/Login'});
+	}
+
+	// if (error.response.status === 422)
+	// {
+	// 	window.app.errors = error.response.data.errors;
+	// }
+
+	//np.done();
+	return Promise.reject(error);
+});
+
+instance.publicURL = serverURL;
+
+export default instance;
